0783ad656f846e4a47be4cd591f1a0e1be856b51
ntaso2051 <normalforce1227@gmail.com>
2020-07-02 22:57:24 +0900

    todolistの追加

---
+++ src/components/TodoList.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div>TodoList</div>
+    <div v-for="task in tasks" :key="task.task">
+      <div class="task">
+        <div class="task">
+          タスク: {{ task.task }} 期限: {{ task.deadLine }}
+          <span v-if="task.isAchived">済</span>
+          <button @click="task.isAchived++" v-if="!task.isAchived">達成</button>
+        </div>
+      </div>
+    </div>
+    <div>
+      <label>
+        タスク
+        <input type="text" v-model="newTask" />
+      </label>
+      <label>
+        期限
+        <input type="text" v-model="newDeadLine" />
+      </label>
+      <button @click="addTask">追加</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoList",
+  data() {
+    return {
+      tasks: []
+    };
+  },
+  methods: {
+    addTask() {
+      this.tasks.push({
+        task: this.newTask,
+        deadLine: this.newDeadLine,
+        isAchived: 0
+      });
+    },
+    achiveTask() {
+      this.tasks.isAchived += 1;
+    }
+  }
+};
+</script>
+
+<style></style>
